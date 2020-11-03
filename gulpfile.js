2382455efd97a8ae3148e54c854c35158787a196
Jaime Niñoles-Manzanera Jimeno <info@jnjsite.com>
2020-11-03 23:45:37 +0100

    Bootstrap from node_modules.

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,14 @@
 'use strict'
 
-const gulp = require("gulp")
-const { parallel } = require("gulp")
-const sass = require("gulp-sass")
-const cleanCss = require("gulp-clean-css")
-const concat = require('gulp-concat')
-const uglify = require('gulp-uglify-es').default
-const sourcemaps = require('gulp-sourcemaps')
+const gulp = require("gulp");
+const { parallel } = require("gulp");
+const sass = require("gulp-sass");
+const cleanCss = require("gulp-clean-css");
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify-es').default;
+const sourcemaps = require('gulp-sourcemaps');
 const rename = require('gulp-rename');
+const fs = require('fs-extra');
 
 function css() {
     return gulp.src('./scss/style.scss')
@@ -16,25 +17,27 @@ function css() {
         //.pipe(cleanCss())
         .pipe(rename('style.css'))
         .pipe(sourcemaps.write())
-        .pipe(gulp.dest('./css'))
+        .pipe(gulp.dest('./css'));
 }
 
 function js() {
+    fs.copy('./node_modules/bootstrap/dist/js/bootstrap.min.js', './js/bootstrap.min.js');
+    fs.copy('./node_modules/bootstrap/dist/js/bootstrap.min.js.map', './js/bootstrap.min.js.map');
     return gulp.src([
-            './js/main.js'
+            './js/main.js',
         ])
         .pipe(sourcemaps.init())
         //.pipe(uglify())
         .pipe(concat('main.min.js'))
         .pipe(sourcemaps.write())
-        .pipe(gulp.dest('./'))
+        .pipe(gulp.dest('./'));
 }
 
 function watchAll() {
-    gulp.watch(['./scss/*', './js/*'], parallel('css', 'js'))
+    gulp.watch(['./scss/*', './js/*'], parallel('css', 'js'));
 }
 
-exports.css = css
-exports.js = js
-exports.watch = watchAll
-exports.default = this.watch
+exports.css = css;
+exports.js = js;
+exports.watch = watchAll;
+exports.default = this.watch;
