c52821f2652d852b3480ac152041aadca0d9b652
Jaime Niñoles-Manzanera Jimeno <info@jnjsite.com>
2020-11-04 22:52:01 +0100

    Container for the content and default sidebar.

--- index.php
+++ index.php
@@ -1,13 +1,31 @@
 <?php
 get_header();
- 
-if ( have_posts() ) : 
-    while ( have_posts() ) : the_post();
-        the_content();
-    endwhile;
-else :
-    _e( 'Sorry, no posts matched your criteria.', 'textdomain' );
-endif;
- 
-get_sidebar();
+?>
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-10">
+
+            <?php
+            if ( have_posts() ) : 
+                while ( have_posts() ) : the_post();
+                    the_content();
+                endwhile;
+            else :
+                _e( 'Sorry, no posts matched your criteria.', 'textdomain' );
+            endif;
+            ?>
+
+        </div>
+        <div class="col-md-2">
+
+            <?php get_sidebar(); ?>
+
+        </div>
+    </div>
+
+</div>
+
+<?php
+
 get_footer();
