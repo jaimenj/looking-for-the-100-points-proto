29f042f759f733d767e04918a9376db10b0464f3
Jaime Niñoles-Manzanera Jimeno <info@jnjsite.com>
2020-11-04 23:24:43 +0100

    Footer post, some links and styles.

--- footer.php
+++ footer.php
@@ -1,7 +1,86 @@
 
-<?php
-wp_footer();
-?>
+
+    <div class="row footer footer-pre">
+        <div class="col-md-2">
+        Lorem ipsum
+        </div>
+        <div class="col-md-2">
+        Lorem ipsum
+        </div>
+        <div class="col-md-2">
+        Lorem ipsum
+        </div>
+        <div class="col-md-2">
+        Lorem ipsum
+        </div>
+        <div class="col-md-2">
+        Lorem ipsum
+        </div>
+        <div class="col-md-2">
+        Lorem ipsum
+        </div>
+    </div>
+    <div class="row footer footer-main">
+        <div class="col-md-12">
+        Lorem ipsum
+        </div>
+    </div>
+    <div class="row footer footer-post">
+        <div class="col-md-6 sign">
+            <p>© <?= date('Y'); ?> JnjSite.com - MIT license</p>
+            <p>Funciona con WordPress - Diseño y programación a medida por Jnj con jQuery, Bootstrap, Popper, Fontawesome..</p>
+        </div>
+        <div class="col-md-6">
+            <div class="socials">
+                <a rel="nofollow noopener noreferrer" 
+                class="social-icon icon-mail" 
+                title="E-mail" 
+                aria-label="E-mail" 
+                href="mailto:info@jnjsite.com" 
+                target="_blank" 
+                data-wpel-link="external"><i class="fas fa-envelope"></i></a>
+                <a rel="noopener noreferrer nofollow external" 
+                class="social-icon icon-linkedin" 
+                title="Linkedin"
+                aria-label="Contáctame en LinkedIn" 
+                href="https://es.linkedin.com/in/jaimeninoles" 
+                target="_blank" 
+                data-wpel-link="external"><i class="fab fa-linkedin"></i></a>
+                <a rel="noopener noreferrer nofollow external" 
+                class="social-icon icon-facebook" 
+                title="Facebook" 
+                aria-label="Sígueme en Facebook" 
+                href="https://www.facebook.com/jaimeninolesinformatico/" 
+                target="_blank" 
+                data-wpel-link="external"><i class="fab fa-facebook"></i></a>
+                <a rel="noopener noreferrer nofollow external" 
+                class="social-icon icon-twitter" 
+                title="Twitter" 
+                aria-label="Sígueme en Twitter" 
+                href="https://twitter.com/jaimeninoles" 
+                target="_blank" 
+                data-wpel-link="external"><i class="fab fa-twitter"></i></a>
+                <a rel="noopener noreferrer nofollow external" 
+                class="social-icon icon-github" 
+                title="Github" 
+                aria-label="Sígueme en Github" 
+                href="https://github.com/jaimenj" 
+                target="_blank" 
+                data-wpel-link="external"><i class="fab fa-github"></i></a>
+                <a rel="nofollow noopener noreferrer" 
+                class="social-icon icon-feed" 
+                title="Suscríbete a mi feed RSS"
+                aria-label="Suscríbete a mi feed RSS" 
+                href="https://jnjsite.com/feed/rss/" 
+                target="_blank" 
+                data-wpel-link="internal"><i class="fas fa-rss"></i></a>
+            </div>
+        </div>
+    </div>
+
+    <?php
+    wp_footer();
+    ?>
 
 </body>
 </html>
--- functions.php
+++ functions.php
@@ -7,7 +7,8 @@ wp_enqueue_style('main', get_template_directory_uri().'/css/main.min.css', ['boo
 wp_enqueue_script('jquery', get_template_directory_uri().'/js/jquery.min.js', [], '', true);
 wp_enqueue_script('popper', get_template_directory_uri().'/js/popper.min.js', ['jquery'], '', true);
 wp_enqueue_script('bootstrap', get_template_directory_uri().'/js/bootstrap.min.js', ['jquery', 'popper'], '', true);
-wp_enqueue_script('main', get_template_directory_uri().'/main.min.js', ['jquery', 'poper', 'bootstrap'], '', true);
+wp_enqueue_script('fontawesome', get_template_directory_uri().'/js/fontawesome.all.min.js', [], '', true);
+wp_enqueue_script('main', get_template_directory_uri().'/main.min.js', [], '', true);
 
 add_theme_support('title-tag');
 add_theme_support('custom-logo', [
