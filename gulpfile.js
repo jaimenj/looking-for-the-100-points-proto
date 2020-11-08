115c5ea0b57f540222c51908faeb371fabc68ad0
Jaime Niñoles-Manzanera Jimeno <info@jnjsite.com>
2020-11-08 18:44:00 +0100

    Improving Gulp workflow.

--- gulpfile.js
+++ gulpfile.js
@@ -1,40 +1,48 @@
-'use strict'
+'use strict';
+
+const useMaximumCompress = false;
+const useSourceMaps = false
 
 const gulp = require("gulp");
 const { parallel } = require("gulp");
 const sass = require("gulp-sass");
 const cleanCss = require("gulp-clean-css");
 const concat = require('gulp-concat');
-const uglify = require('gulp-uglify-es').default;
+const terser = require('gulp-terser');
+const strip = require('gulp-strip-comments');
+const removeEmptyLines = require('gulp-remove-empty-lines');
 const sourcemaps = require('gulp-sourcemaps');
 const rename = require('gulp-rename');
 const fs = require('fs-extra');
+const gulpif = require('gulp-if');
 
 function css() {
     return gulp.src('./src/scss/main.scss')
-        .pipe(sourcemaps.init())
+        .pipe(gulpif(useSourceMaps, sourcemaps.init()))
         .pipe(sass())
-        //.pipe(cleanCss())
+        .pipe(cleanCss({ format: 'keep-breaks' }))
         .pipe(rename('main.min.css'))
-        .pipe(sourcemaps.write())
+        .pipe(gulpif(useSourceMaps, sourcemaps.write()))
         .pipe(gulp.dest('./css'));
 }
 
 function js() {
-    return gulp.src([
-            './src/js/main.js'
-        ])
-        .pipe(sourcemaps.init())
-        //.pipe(uglify())
+    let javascriptSources = [
+        './src/js/main.js'
+    ];
+    return gulp.src(javascriptSources)
+        .pipe(gulpif(useSourceMaps, sourcemaps.init()))
+        .pipe(strip())
+        .pipe(removeEmptyLines())
+        .pipe(gulpif(useMaximumCompress, terser()))
         .pipe(concat('main.min.js'))
-        .pipe(sourcemaps.write())
+        .pipe(gulpif(useSourceMaps, sourcemaps.write()))
         .pipe(gulp.dest('./js/'));
 }
 
 function watchAll() {
     fs.copy('./node_modules/bootstrap/dist/css/bootstrap.min.css', './css/bootstrap.min.css');
     fs.copy('./node_modules/bootstrap/dist/css/bootstrap.min.css.map', './css/bootstrap.min.css.map');
-
     fs.copy('./node_modules/jquery/dist/jquery.min.js', './js/jquery.min.js');
     fs.copy('./node_modules/popper.js/dist/umd/popper.min.js', './js/popper.min.js');
     fs.copy('./node_modules/popper.js/dist/umd/popper.min.js.map', './js/popper.min.js.map');
